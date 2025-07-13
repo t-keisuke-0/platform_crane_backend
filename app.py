861b93b8480f8e1fbc37fe9c90b1ab1312c6f60e
t-keisuke-0 <kasakokey0728@gmail.com>
2025-07-13 19:05:40 +0900

    render deploy test

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def echo():
     data = request.get_json()
     return jsonify({'you_sent': data})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
