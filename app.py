8567b3ba92ed3f786f8bbb2ac600b108c2c694bc
t-keisuke-0 <kasakokey0728@gmail.com>
2025-08-28 21:46:12 +0900

    fastAPI 切り替え

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 
-app = Flask(__name__)
-CORS(app, origins=[
+app = FastAPI()
+
+origins = [
     "http://localhost:3000",
     "https://platform-crane-frontend.onrender.com"
-]) # 許可するオリジンを指定
+]
 
-@app.route('/')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
 def hello():
-    return 'Hello, Flask!'
+    return "Hello, FastAPI!"
 
-@app.route('/api/greet', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'World')
-    return jsonify({'message': f'Hello, {name}!'})
+@app.get("/api/greet")
+def greet(name: str = "World"):
+    return {"message": f"Hello, {name}!"}
 
-@app.route('/api/echo', methods=['POST'])
-def echo():
-    data = request.get_json()
-    return jsonify({'you_sent': data})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.post("/api/echo")
+async def echo(request: Request):
+    data = await request.json()
+    return {"you_sent": data}
